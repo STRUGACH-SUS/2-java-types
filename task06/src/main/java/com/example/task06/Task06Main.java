9cdda78e66a77d25f46a8c9961448f4e0495515e
stryg <mitakervanov@gmail.com>
2026-09-08 00:43:24 +0500

    Выполнил 6 задание

--- task06/src/main/java/com/example/task06/Task06Main.java
+++ task06/src/main/java/com/example/task06/Task06Main.java
@@ -3,19 +3,13 @@ package com.example.task06;
 public class Task06Main {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int sum = Math.abs(x + y);
+        String s = Integer.toString(sum);
+        return s.length();
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
+        int result = solution(625, 5324);
         System.out.println(result);
-        */
     }
-
 }
